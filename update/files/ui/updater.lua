aa234655a23610f90002eca777dd09eb783e6cc9
たまさん <58621773+kazu55@users.noreply.github.com>
2023-01-03 15:08:34 +0900

    更新

--- update/files/ui/updater.lua
+++ update/files/ui/updater.lua
@@ -26,7 +26,15 @@ begin:onClick(function()
     fs.makeDir("/backup")
     for i = 1, #update_downloadfiles do
         fs.delete(update_downloadfiles[i][2])
-        shell.run("wget ".. update_downloadfiles[i][1] .. " " .. update_downloadfiles[i][2])
+        local data = http.get(update_downloadfiles[i][1], update_downloadfiles[i][3] or false)
+        if not update_downloadfiles[i][3] then
+            local file = fs.open(update_downloadfiles[i][2], "w")
+        else
+            local file = fs.open(update_downloadfiles[i][2], "wb")
+        end
+        file.write(data.readAll())
+        file.close()
+        data.close()
     end
     os.reboot()
 end
