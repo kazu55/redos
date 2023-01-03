8a601aa6bd504234b1e354d92bff334c4f838170
たまさん <58621773+kazu55@users.noreply.github.com>
2023-01-03 17:55:00 +0900

    upd

--- installer.lua
+++ installer.lua
@@ -4,12 +4,6 @@ shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/v
 shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/download-files.txt")
 
 shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/version.txt")
-if runningversion < newver then
-    print("Getting update files list")
-    logwrite("Getting update-files list")
-    shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/download-files.txt")
-end
-
 if not update_downloadfiles then
     error("This version is latest", 2)
 else
