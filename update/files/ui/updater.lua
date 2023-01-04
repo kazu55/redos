2f2c36d5e8487f3ac2bf414b5a31aad1ef437a91
たまさん <58621773+kazu55@users.noreply.github.com>
2023-01-04 15:32:25 +0900

    Update updater.lua

--- update/files/ui/updater.lua
+++ update/files/ui/updater.lua
@@ -1,2 +1,13 @@
-shell.setDir("/")
-shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/installer.lua")
+shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/version.txt")
+if runningversion < newver then
+    print("Getting update files list")
+    logwrite("Getting update-files list")
+    shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/download-files.txt")
+    if not update_downloadfiles then
+      error("Download files not Found", 2)
+    else
+      shell.setDir("/")
+      shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/installer.lua")
+    end
+end
+error("Not Update released")
