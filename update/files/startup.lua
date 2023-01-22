c52f1a3d5c0cae1879a00046891c755357d022ba
redman <gakisen55@gmail.com>
2023-01-22 18:01:55 +0900

    term.setPaletteColorをすべて削除しました。

--- update/files/startup.lua
+++ update/files/startup.lua
@@ -1,14 +1,5 @@
-_G.runningversion = 202315
+_G.runningversion = 202316
 _G.versiontype = "release"
-term.setPaletteColor(colors.black, 0, 0, 0)
-term.setPaletteColor(colors.red, 1, 0, 0)
-term.setPaletteColor(colors.blue, 0, 0, 1)
-term.setPaletteColor(colors.green, 0, 1, 0)
-term.setPaletteColor(colors.white, 1, 1, 1)
-term.setPaletteColor(colors.yellow, 1, 1, 0)
-term.setPaletteColor(colors.cyan, 0, 1, 1)
-term.setPaletteColor(colors.gray, 0.25, 0.25, 0.25)
-term.setPaletteColor(colors.lightGray, 0.75, 0.75, 0.75)
 
 os.loadAPI("/ui/api/dialog/dialog.lua")
 
