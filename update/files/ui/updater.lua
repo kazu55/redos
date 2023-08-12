e1ce6267498fa93d8ee957b2db5c08f585acf0f2
たまさん <58621773+kazu55@users.noreply.github.com>
2023-08-12 11:56:51 +0900

    Update updater.lua

--- update/files/ui/updater.lua
+++ update/files/ui/updater.lua
@@ -1,7 +1,7 @@
 shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/version.txt")
 if runningversion < newver then
     shell.setDir("/")
-    shell.run("wget run https://raw.githubusercontent.com/kazu55/redos/main/update/run.lua /update/run.lua")
+    shell.run("wget https://raw.githubusercontent.com/kazu55/redos/main/update/run.lua /update/run.lua")
     os.reboot()
 end
 error("Not Update released")
